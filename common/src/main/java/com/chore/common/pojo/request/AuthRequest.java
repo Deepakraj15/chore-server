a76c2e4f04c77eea73995fcee3e4ee47dfbb0ed2
Deepakraj15 <deepakraj15112002@gmail.com>
2025-05-01 17:14:56 +0530

    included docker and modified pom.xml for building and deployment purposes

---
+++ common/src/main/java/com/chore/common/pojo/request/AuthRequest.java
@@ -0,0 +1,9 @@
+package com.chore.common.pojo.request;
+
+import lombok.Data;
+
+@Data
+public class AuthRequest {
+    private String username;
+    private String password;
+}
--- services/auth-service/src/main/java/com/chore/auth_service/controller/AuthController.java
+++ services/auth-service/src/main/java/com/chore/auth_service/controller/AuthController.java
@@ -1,13 +1,21 @@
 package com.chore.auth_service.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.chore.common.pojo.request.AuthRequest;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    
+    @PostMapping("/auth-token")
+    public String getAuthToken(@RequestBody AuthRequest authRequest) {
+        return "dfs";
+    }
     @GetMapping("getStatus")
     public String getStatus() {
         return "Status is up";
