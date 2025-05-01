e9daf633e945fdf975d6b2ff8305bbaad71db67e
Deepakraj15 <deepakraj15112002@gmail.com>
2025-05-01 21:34:06 +0530

    modified security config for now

--- services/auth-service/src/main/java/com/chore/auth_service/configuration/SecurityConfig.java
+++ services/auth-service/src/main/java/com/chore/auth_service/configuration/SecurityConfig.java
@@ -1,33 +1,23 @@
 package com.chore.auth_service.configuration;
 
-import com.chore.auth_service.service.CustomOAuth2UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    private CustomOAuth2UserService customOAuth2UserService;
-
+    //TODO : Implement JWT authentication and authorization
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**", "/oauth2/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .oauth2Login(oauth2 -> oauth2
-                .loginPage("/login") // optional if you're using a custom login page
-                .userInfoEndpoint(userInfo -> userInfo
-                    .userService(customOAuth2UserService)
-                )
-            );
+                .authorizeHttpRequests(auth -> auth
+                                .anyRequest().permitAll() 
+                ).httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
 
         return http.build();
     }
